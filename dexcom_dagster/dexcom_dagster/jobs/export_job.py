d382b7e5202108d32caf4af06bce174cdaed13e0
Kyle Jamieson <krjamies@gmail.lcom>
2025-06-21 21:57:06 -0500

    Rename export job to standardize naming convention

--- dexcom_dagster/dexcom_dagster/jobs/export_job.py
+++ dexcom_dagster/dexcom_dagster/jobs/export_job.py
@@ -1,6 +1,6 @@
 from dagster import define_asset_job
 
 export_job = define_asset_job(
-    name="export_glucose_to_parquet_and_upload_job",
+    name="export_job",
     selection=["export_glucose_to_parquet_and_upload"]
 )
